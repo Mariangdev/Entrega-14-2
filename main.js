401bd58970c33803720e1b9444a4e0bf3d41cf46
Marian <95725615+Mariangdev@users.noreply.github.com>
2022-02-21 21:54:19 -0300

    DOM

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-
-const letras = ["a", "b", "c", "d"];
-
-letras.forEach((e) => console.log(e)); // Devuelve 'a' / 'b' / 'c' / 'd'
-letras.forEach((e, i) => console.log(e, i)); // Devuelve 'a' 0 / 'b' 1 / 'c' 2 / 'd' 3
-letras.forEach((e, i, a) => console.log(a[0])); // Devuelve 'a' / 'a' / 'a' / 'a'  
+function mostrarValores() {
+    let formulario = document.forms['formulario'];
+    let texto = '';
+    for(let elemento of formulario) {
+        texto += elemento.value + '<br/>';
+    }
+    document.getElementById('valores').innerHTML = texto;
+}
